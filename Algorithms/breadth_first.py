b6023668ff5e90a9f94ee81a970fdd86a406db31
unknown <suryachallass@gmail.com>
2024-08-07 05:20:22 +0800

    the lord has spoken

--- Algorithms/breadth_first.py
+++ Algorithms/breadth_first.py
@@ -20,11 +20,11 @@ class Deque:
 class BreadthFirst_MazeSolver:
     def __init__(self, pos_x, pos_y, maze):
         self.maze = maze
-        self.start_x = pos_x
-        self.start_y = pos_y
+        self.pos_x = pos_x
+        self.pos_y = pos_y
         self.queue = Deque()  # Use the custom Deque class
-        self.queue.append((self.start_x, self.start_y))
-        self.visited = set([(self.start_x, self.start_y)])
+        self.queue.append((self.pos_x, self.pos_y))
+        self.visited = set([(self.pos_x, self.pos_y)])
         self.numbered_path_nodes = []
         self.parent = {}  # To keep track of the path
         self.dx = [0, 1, 0, -1]
@@ -34,7 +34,15 @@ class BreadthFirst_MazeSolver:
         """Solve the maze using Breadth-First Search (BFS)."""
         end_found = False
         end_position = None
-
+        start_x, start_y = None, None
+        for my, row in enumerate(self.maze):
+            for mx, cell in enumerate(row):
+                if cell == 's':
+                    start_x, start_y = mx, my
+                    break
+            if start_x is not None:
+                break
+            
         while len(self.queue) > 0:
             x, y = self.queue.popleft()
 
@@ -57,20 +65,20 @@ class BreadthFirst_MazeSolver:
                     self.parent[(nx, ny)] = (x, y)
 
         if end_found:
-            self.trace_path(end_position)
+            self.trace_path(end_position, start_x, start_y)
 
         return self.maze, self.numbered_path_nodes
 
-    def trace_path(self, end_position):
+    def trace_path(self, end_position, start_x, start_y):
         """Trace back from the end position to the start position to build the path."""
         x, y = end_position
         path_stack = []
 
-        while (x, y) != (self.start_x, self.start_y):
+        while (x, y) != (self.pos_x, self.pos_y):
             path_stack.append((x, y))
             x, y = self.parent[(x, y)]
 
-        path_stack.append((self.start_x, self.start_y))
+        path_stack.append((self.pos_x, self.pos_y))
 
         while path_stack:
             px, py = path_stack.pop()
@@ -79,7 +87,7 @@ class BreadthFirst_MazeSolver:
                 self.maze[py][px] = '-'  # Mark the shortest path
 
         # Mark the start position last to keep it as 's'
-        self.maze[self.start_y][self.start_x] = 's'
+        self.maze[start_y][start_x] = 's'
 
     def print_maze(self):
         """Print the current state of the maze."""
--- Algorithms/depth_first.py
+++ Algorithms/depth_first.py
@@ -1,9 +1,9 @@
 class DepthFirst_MazeSolver:
     def __init__(self, pos_x, pos_y, maze):
         self.maze = maze
-        self.start_x = pos_x
-        self.start_y = pos_y
-        self.stack = [(self.start_x, self.start_y)]
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.stack = [(self.pos_x, self.pos_y)]
         self.visited = set(self.stack)
         self.numbered_path_nodes = []
         self.dx = [0, 1, 0, -1]
@@ -11,6 +11,15 @@ class DepthFirst_MazeSolver:
 
     def solve_maze_dfs(self):
         """Solve the maze using Depth First Search (DFS)."""
+        start_x, start_y = None, None
+        for my, row in enumerate(self.maze):
+            for mx, cell in enumerate(row):
+                if cell == 's':
+                    start_x, start_y = mx, my
+                    break
+            if start_x is not None:
+                break
+            
         while self.stack:
             x, y = self.stack[-1]
 
@@ -39,7 +48,7 @@ class DepthFirst_MazeSolver:
                     self.numbered_path_nodes.pop()
 
         # Mark the start position with 's'
-        self.maze[self.start_y][self.start_x] = 's'
+        self.maze[start_y][start_x] = 's'
         
         return self.maze, self.numbered_path_nodes
 
